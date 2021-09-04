e790bdfda1cb2eb836a404ec13afca8b1628b937
Michael Goulet <michael@errs.io>
2021-09-04 12:13:55 -0700

    Implement some iterators for Punctuated

--- crates/camp_parse/src/lib.rs
+++ crates/camp_parse/src/lib.rs
@@ -1,12 +1,14 @@
 #![feature(never_type)]
 
 mod result;
+mod punctuated;
 
 use camp_diagnostic::{err, Result};
 use camp_lex::{tok::Token as LexToken, TokenBuffer};
 use camp_files::Span;
 
 use result::ParseError;
+pub use punctuated::Punctuated;
 
 #[derive(Clone)]
 pub struct ParseContext<'token> {
@@ -173,70 +175,4 @@ pub trait Peek: Sized {
     fn peek(input: &ParseContext<'_>) -> bool;
 
     fn name() -> &'static str;
-}
-
-#[derive(Debug)]
-pub struct Punctuated<T, S> {
-    items: Vec<(T, S)>,
-    last: Option<Box<T>>,
-}
-
-impl<T, S> Punctuated<T, S> {
-    pub fn new() -> Punctuated<T, S> {
-        Punctuated {
-            items: vec![],
-            last: None,
-        }
-    }
-
-    pub fn push(&mut self, t: T) {
-        assert!(self.last.is_none());
-        self.last = Some(Box::new(t));
-    }
-
-    pub fn push_punct(&mut self, s: S) {
-        let t = self.last.take().unwrap();
-        self.items.push((*t, s))
-    }
-
-    pub fn pop_punct(&mut self) -> S {
-        assert!(self.last.is_none());
-        let (t, s) = self.items.pop().unwrap();
-        self.last = Some(Box::new(t));
-        s
-    }
-
-    pub fn len(&self) -> usize {
-        self.items.len() + (self.last.is_some() as usize)
-    }
-
-    pub fn trailing(&self) -> bool {
-        // If there's at least one item, and not a item at the end
-        !self.items.is_empty() && self.last.is_none()
-    }
-
-    pub fn unwrap_one(self) -> T {
-        assert!(self.items.is_empty());
-        *self.last.unwrap()
-    }
-
-    pub fn map<T2>(self, mut f: impl FnMut(T) -> T2) -> Punctuated<T2, S> {
-        Punctuated {
-            items: self.items.into_iter().map(|(t, s)| (f(t), s)).collect(),
-            last: self.last.map(|t| Box::new(f(*t))),
-        }
-    }
-
-    pub fn first(&self) -> Option<&T> {
-        self.items
-            .first()
-            .map(|(t, _)| t)
-            .or_else(|| self.last.as_deref())
-    }
-
-    pub fn last(&self) -> Option<&T> {
-        self.last
-            .as_deref()
-            .or_else(|| self.items.last().map(|(t, _)| t))
-    }
 }
---
+++ crates/camp_parse/src/punctuated.rs
@@ -0,0 +1,73 @@
+#[derive(Debug)]
+pub struct Punctuated<T, S> {
+    items: Vec<(T, S)>,
+    last: Option<Box<T>>,
+}
+
+impl<T, S> Punctuated<T, S> {
+    pub fn new() -> Punctuated<T, S> {
+        Punctuated {
+            items: vec![],
+            last: None,
+        }
+    }
+
+    pub fn push(&mut self, t: T) {
+        assert!(self.last.is_none());
+        self.last = Some(Box::new(t));
+    }
+
+    pub fn push_punct(&mut self, s: S) {
+        let t = self.last.take().unwrap();
+        self.items.push((*t, s))
+    }
+
+    pub fn pop_punct(&mut self) -> S {
+        assert!(self.last.is_none());
+        let (t, s) = self.items.pop().unwrap();
+        self.last = Some(Box::new(t));
+        s
+    }
+
+    pub fn len(&self) -> usize {
+        self.items.len() + (self.last.is_some() as usize)
+    }
+
+    pub fn trailing(&self) -> bool {
+        // If there's at least one item, and not a item at the end
+        !self.items.is_empty() && self.last.is_none()
+    }
+
+    pub fn unwrap_one(self) -> T {
+        assert!(self.items.is_empty());
+        *self.last.unwrap()
+    }
+
+    pub fn map<T2>(self, mut f: impl FnMut(T) -> T2) -> Punctuated<T2, S> {
+        Punctuated {
+            items: self.items.into_iter().map(|(t, s)| (f(t), s)).collect(),
+            last: self.last.map(|t| Box::new(f(*t))),
+        }
+    }
+
+    pub fn first(&self) -> Option<&T> {
+        self.items
+            .first()
+            .map(|(t, _)| t)
+            .or_else(|| self.last.as_deref())
+    }
+
+    pub fn last(&self) -> Option<&T> {
+        self.last
+            .as_deref()
+            .or_else(|| self.items.last().map(|(t, _)| t))
+    }
+
+    pub fn items(&self) -> impl Iterator<Item = &'_ T> {
+        self.items.iter().map(|(t, _)| t).chain(self.last.iter().map(|t| t.as_ref()))
+    }
+
+    pub fn into_items(self) -> impl Iterator<Item = T> {
+        self.items.into_iter().map(|(t, _)| t).chain(self.last.map(|t| *t))
+    }
+}
